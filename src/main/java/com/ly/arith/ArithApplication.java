37e7a127dd7e3b85eb7452c1f0dc4e1c237d1101
tuoer1995 <2413558497@qq.com>
2019-06-24 23:41:02 +0800

    Initial commit

---
+++ src/main/java/com/ly/arith/ArithApplication.java
@@ -0,0 +1,13 @@
+package com.ly.arith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run ( ArithApplication.class , args );
+    }
+
+}
---
+++ src/main/java/com/ly/arith/sort/Sort.java
@@ -0,0 +1,10 @@
+package com.ly.arith.sort;
+
+/**
+ * TODO
+ *
+ * @author tuoer
+ * @date 2019/6/24 23:39
+ */
+public interface Sort {
+}
